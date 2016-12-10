fbe64256b61fdc0d4e015cc24e6bc4e519cbb242
Ming <ming.george.li@gmail.com>
2016-12-10 11:52:21 +0000

    new commit

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ def max_pool_2x2(x):
 
 
 # First Convolution Layer
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([5, 5, 1, 128])
+b_conv1 = bias_variable([128])
 
 x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], 1])
 
@@ -67,17 +67,17 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # Second layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 128, 256])
+b_conv2 = bias_variable([256])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # Densely connected layer
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([7 * 7 * 256, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 256])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # Dropout
@@ -91,7 +91,7 @@ b_fc2 = bias_variable([n])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=0.0005, beta1=.9, beta2=.999).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -114,7 +114,7 @@ def main(loop_num=0):
         x_batch, y_batch = zip(*batch[2])
         x_batch = np.array(x_batch)
         y_batch = np.array(y_batch)
-        if i % 100 == 0:
+        if i % 200 == 0:
             train_accuracy = accuracy.eval(feed_dict={x: valid_x, y_: valid_y, keep_prob: 1.0}, session=sess)
             print("loop {3}, epoch {2}, step {0}, training accuracy {1:.4f}".format(i, train_accuracy, e, loop_num))
             recent_100.append(train_accuracy)
@@ -133,7 +133,7 @@ def main(loop_num=0):
 
 cross_val = False
 
-kf_iterator = model_selection.StratifiedKFold(n_splits=24, shuffle=True)  # Stratified
+kf_iterator = model_selection.StratifiedKFold(n_splits=21, shuffle=True)  # Stratified
 count = 0
 
 for train_index, valid_index in kf_iterator.split(data.ix[:, 1:], data.ix[:, 0]):
