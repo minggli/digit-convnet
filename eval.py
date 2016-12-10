3f5ddd3ce5f70ee280e3ccc96ab701ee3a56b845
Ming <ming.george.li@gmail.com>
2016-12-10 15:30:58 +0000

    new commit

--- eval.py
+++ eval.py
@@ -18,6 +18,15 @@ input_shape = (np.int32(test.shape[1] ** 0.5), np.int32(test.shape[1] ** 0.5))
 m = test.shape[1]  # num of flat array
 n = 10
 
+# params
+
+dir_path = 'input/'
+model_path = 'models/'
+train, label, data = extract('input/train.csv')
+input_shape = (np.int32(train.shape[1] ** 0.5), np.int32(train.shape[1] ** 0.5))
+m = train.shape[1]  # num of flat array
+n = len(set(label.columns))
+
 # load image into tensor
 
 sess = tf.Session()
@@ -54,8 +63,8 @@ def max_pool_2x2(x):
 
 
 # First Convolution Layer
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([5, 5, 1, 64])
+b_conv1 = bias_variable([64])
 
 x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], 1])
 
@@ -63,17 +72,17 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # Second layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 64, 128])
+b_conv2 = bias_variable([128])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # Densely connected layer
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([7 * 7 * 128, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 128])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # Dropout
@@ -87,7 +96,7 @@ b_fc2 = bias_variable([n])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-train_step = tf.train.AdamOptimizer(learning_rate=1e-3, beta1=.9, beta2=.999).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=.005, beta1=.9, beta2=.999).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
