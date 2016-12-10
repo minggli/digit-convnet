eeb4173b4568a0c00cc9a864b397e8b4f3d9c053
Ming <ming.george.li@gmail.com>
2016-12-10 09:21:26 +0000

    new commit

--- eval.py
+++ eval.py
@@ -54,7 +54,7 @@ def max_pool_2x2(x):
 
 
 # First Convolution Layer
-W_conv1 = weight_variable([3, 3, 1, 64])
+W_conv1 = weight_variable([5, 5, 1, 64])
 b_conv1 = bias_variable([64])
 
 x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], 1])
@@ -63,7 +63,7 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # Second layer
-W_conv2 = weight_variable([3, 3, 64, 128])
+W_conv2 = weight_variable([5, 5, 64, 128])
 b_conv2 = bias_variable([128])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
