a6c8d7f4ca77b9b508e0e170a2707bc7f08285cc
Ming <ming.george.li@gmail.com>
2016-12-17 03:51:49 +0000

    new commit

--- main.py
+++ main.py
@@ -112,14 +112,14 @@ if __name__ == '__main__':
     x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], 1])
 
     with tf.name_scope('hidden_layer_1'):
-        W_conv1 = weight_variable([5, 5, 1, 32])
+        W_conv1 = weight_variable([3, 3, 1, 32])
         b_conv1 = bias_variable([32])
 
         h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = max_pool(h_conv1)
 
     with tf.name_scope('hidden_layer_2'):
-        W_conv2 = weight_variable([5, 5, 32, 64])
+        W_conv2 = weight_variable([3, 3, 32, 64])
         b_conv2 = bias_variable([64])
 
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
