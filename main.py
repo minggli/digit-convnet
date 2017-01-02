f89da97519802af65a41a80f51887b405f17bfd1
Ming <ming.george.li@gmail.com>
2017-01-02 17:28:27 +0000

    new commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from utilities import extract, batch_iter, generate_training_set
 from sklearn import metrics, linear_model, model_selection, preprocessing
 import sys
 import pandas as pd
-
+from warnings import filterwarnings
+filterwarnings(action='ignore')
 import os
 
 # coding: utf-8
@@ -99,7 +100,17 @@ if __name__ == '__main__':
         regressors = np.column_stack(
             (np.ones(shape=train.shape[0]), train.apply(preprocessing.scale, axis=0, with_mean=True, with_std=True))
         )
-        regressand = np.array(label)
+        regressand = np.array(data['label'])
+
+        lr = linear_model.LogisticRegression(fit_intercept=False)
+        lr.fit(regressors, regressand)
+
+        KF_generator = model_selection.StratifiedKFold(n_splits=5, shuffle=False)
+        avg_scores = model_selection.cross_val_score(
+            lr, regressors, regressand, scoring=metrics.accuracy_score, cv=KF_generator)
+
+        print('Using given features by Kaggle, Logistic Regression model accuracy is: ', end='')
+        print('{0:.2f}%'.format(100 * np.mean(avg_scores)), flush=True, end='\n')
 
     sess = tf.Session()
 
