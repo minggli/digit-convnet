04618804059e6e0540a583496da7075398b8b832
Ming <ming.george.li@gmail.com>
2017-01-05 13:55:44 +0000

    new commit

--- ensemble_sigmoid.py
+++ ensemble_sigmoid.py
@@ -1,6 +1,6 @@
 import numpy as np
 from utilities import extract, batch_iter, generate_training_set
-from sklearn import metrics, linear_model, model_selection, preprocessing
+from sklearn import metrics, linear_model, model_selection, preprocessing, ensemble
 import sys
 import pandas as pd
 
@@ -11,18 +11,27 @@ train, label, data = extract(INPUT_PATH + 'train.csv', target='label')
 
 
 regressors = np.column_stack(
-    (np.ones(shape=train.shape[0]), train.apply(preprocessing.scale, axis=0, with_mean=False, with_std=True))
+    (np.ones(shape=train.shape[0]), train.apply(preprocessing.scale, axis=0, with_mean=True, with_std=True))
 )
 
 regressand = np.array(data['label'])
 
 lr = linear_model.LogisticRegression(fit_intercept=False)
-lr.fit(regressors, regressand)
-print(lr.coef_)
+# lr.fit(regressors, regressand)
+# print(lr.coef_)
 
-KF_generator = model_selection.KFold(n_splits=5, shuffle=False)
+KF_generator = model_selection.StratifiedKFold(n_splits=3, shuffle=False)
 avg_scores = model_selection.cross_val_score(
     lr, regressors, regressand, scoring='accuracy', cv=KF_generator)
 
 print('Using given features by Kaggle, Logistic Regression model accuracy is: ', end='')
-print('{0:.2f}%'.format(100 * np.mean(avg_scores)), flush=True, end='\n')
+print('{1} averaging in {0:.2f}%'.format(100 * np.mean(avg_scores), avg_scores), flush=True, end='\n')
+
+forest = ensemble.RandomForestClassifier(n_estimators=5, max_leaf_nodes=500, max_depth=None)
+
+avg_scores = model_selection.cross_val_score(
+    forest, regressors, regressand, scoring='accuracy', cv=KF_generator)
+
+print('Using given features by Kaggle, Random Forest model accuracy is: ', end='')
+print('{1} averaging in {0:.2f}%'.format(100 * np.mean(avg_scores), avg_scores), flush=True, end='\n')
+
